11118c8b7a0582ae0c5213df8debb28b43ae71be
LeoGoettlinger <95125536+LeoGoettlinger@users.noreply.github.com>
2026-07-29 09:39:27 +0000

    v8.1

--- Korrrektur250330.py
+++
@@ -1,6 +0,0 @@
-# Aufgabe 1
-class Spieler:
-    self.bild = "spieler.png"
-    self.leben = 3
-# Aufgabe 2
-...
--- KorrrekturTest2500202.py
+++
@@ -1,3 +0,0 @@
-# Aufgabe 1
-# SpriteList und Iterable[SpriteList]
-# Ist sinnvoll, da man so flexibel dahingehend ist, ob man alle Blöcke, die als Plattformen/Leitern/Walls agieren sollen, in einer einzigen
